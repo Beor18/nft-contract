36bf7da2f1510d0f544681f8a6eb0b8d7d254223
Fernando Lopez <fer.ragnar19@gmail.com>
2021-11-15 11:27:53 -0300

    se define struct

--- crowdfunding.sol
+++ crowdfunding.sol
@@ -3,14 +3,21 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract CrowdFunding {
-    string public id;
-    string public name;
-    string public description;
-    
-    address payable public author; // variable address payable maneja la cartera que vamos a recibir fondos
-    uint public state; // estado del proyeto open o close
-    uint public funds;
-    uint public fundraisingGoal;
+
+    // Se define la estructura
+    struct Project {
+        string id;
+        string name;
+        string description;
+
+        address payable author; // variable address payable maneja la cartera que vamos a recibir fondos
+        uint state; // estado del proyeto open o close
+        uint funds;
+        uint fundraisingGoal;
+    }
+
+    // la estructura se pone como public
+    Project public project;
 
     // Eventos
     // Permite conectar lo que psa dentro de la Blockchain con el exterior porque
@@ -28,34 +35,30 @@ contract CrowdFunding {
     );
 
     constructor(string memory _id, string memory _name, string memory _description, uint _fundraisingGoal) {
-        id = _id;
-        name = _name;
-        description = _description;
-        fundraisingGoal = _fundraisingGoal;
-        author = payable(msg.sender);
+        project = Project(_id, _name, _description, payable(msg.sender), 0, 0, _fundraisingGoal);
     }
 
     modifier isAuthor() {
-        require(author == msg.sender, "Necesitas ser el autor del proyecto");
+        require(project.author == msg.sender, "Necesitas ser el autor del proyecto");
         _;
     }
 
     modifier isNotAuthor() {
-        require(author != msg.sender, "Como autor no puedes financiar tu propio proyecto");
+        require(project.author != msg.sender, "Como autor no puedes financiar tu propio proyecto");
         _;
     }
 
     function fundProject() public payable isNotAuthor {
-        require(state != 1, "El proyecto no puede recibir fondos"); // valida si el estado es diferente sino envia mensaje error
+        require(project.state != 1, "El proyecto no puede recibir fondos"); // valida si el estado es diferente sino envia mensaje error
         require(msg.value > 0, "El valor del fondo debe ser mayor que 0"); // valida si el valor de eth que envia el user es mayor a 0 sino mensaje error
-        author.transfer(msg.value);
-        funds += msg.value;
-        emit ProjectFunded(id, msg.value);
+        project.author.transfer(msg.value);
+        project.funds += msg.value;
+        emit ProjectFunded(project.id, msg.value);
     }
 
     function changeProjectState(uint newState) public isAuthor {
-        require(state != newState, "El nuevo estado debe ser diferente"); // valida si el estado actual es diferente al nuevo estado
-        state = newState;
-        emit ProjectStateChanged(id, newState);
+        require(project.state != newState, "El nuevo estado debe ser diferente"); // valida si el estado actual es diferente al nuevo estado
+        project.state = newState;
+        emit ProjectStateChanged(project.id, newState);
     }
 }
