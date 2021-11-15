b00b7d71dae78eed325078d0928f0bccffc9570f
Fernando Lopez <fer.ragnar19@gmail.com>
2021-11-15 10:15:09 -0300

    validacion de errores

--- crowdfunding.sol
+++ crowdfunding.sol
@@ -8,7 +8,7 @@ contract CrowdFunding {
     string public description;
     
     address payable public author; // variable address payable maneja la cartera que vamos a recibir fondos
-    string public state = 'Opened';
+    uint public state; // estado del proyeto open o close
     uint public funds;
     uint public fundraisingGoal;
 
@@ -24,7 +24,7 @@ contract CrowdFunding {
 
     event ProjectStateChanged(
         string id,
-        string state
+        uint state
     );
 
     constructor(string memory _id, string memory _name, string memory _description, uint _fundraisingGoal) {
@@ -46,12 +46,15 @@ contract CrowdFunding {
     }
 
     function fundProject() public payable isNotAuthor {
+        require(state != 1, "El proyecto no puede recibir fondos"); // valida si el estado es diferente sino envia mensaje error
+        require(msg.value > 0, "El valor del fondo debe ser mayor que 0"); // valida si el valor de eth que envia el user es mayor a 0 sino mensaje error
         author.transfer(msg.value);
         funds += msg.value;
         emit ProjectFunded(id, msg.value);
     }
 
-    function changeProjectState(string calldata newState) public isAuthor {
+    function changeProjectState(uint newState) public isAuthor {
+        require(state != newState, "El nuevo estado debe ser diferente"); // valida si el estado actual es diferente al nuevo estado
         state = newState;
         emit ProjectStateChanged(id, newState);
     }
