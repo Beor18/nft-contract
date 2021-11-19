58213fef692d2949decf87defb5a3434673ce140
Fernando Lopez <fer.ragnar19@gmail.com>
2021-11-18 22:41:36 -0300

    add adn nft and fixs

--- contracts/AmazonaVerse.sol
+++ contracts/AmazonaVerse.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Base64.sol";
+import "./AmazonaVerseADN.sol";
 
 contract AmazonaVerse is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
